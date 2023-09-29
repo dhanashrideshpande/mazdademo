32578086ae076e6d0f5fcc9b7407abc880aaffe2
Dhanashri Bhagwan <dhanashri.bhagwan@cantire.com>
2023-09-29 15:12:56 +0530

    changes for cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,11 +22,12 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const h2 = main.querySelector('h2');
+  const h6 = main.querySelector('h6');
   const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h2, h1] }));
+    section.append(buildBlock('hero', { elems: [picture, h2, h1, h6] }));
     main.prepend(section);
   }
 }
