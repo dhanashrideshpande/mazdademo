0f1676be79b778e92b02265ec760b2f54ae780d9
Dhanashri Bhagwan <dhanashri.bhagwan@cantire.com>
2023-09-29 12:48:41 +0530

    changes for hero

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,6 +26,7 @@ function buildHeroBlock(main) {
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
     section.append(buildBlock('hero', { elems: [picture, h1] }));
+     section.append(buildBlock('hero', { elems: [picture, h2] }));
     main.prepend(section);
   }
 }
